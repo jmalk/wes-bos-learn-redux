78a623141c34d89282453af5892408f0addab2c3
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2021-04-21 16:59:25 +0100

    04: Create redux store

---
+++ learn-redux/client/store.js
@@ -0,0 +1,21 @@
+import { createStore, compose } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+// Import the root reducer
+// TODO: Index needed?
+import rootReducer from './reducers/index';
+
+// Default data so we don't have to worry about interacting with an API for tutorial
+import comments from './data/comments';
+import posts from './data/posts';
+
+// Create object for default data
+const defaultState = {
+  posts,
+  comments
+};
+
+export const store = createStore(rootReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory, store);
